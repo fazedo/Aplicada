b18960a307291813fb1a2d156d54db34bd76698d
Fabio Souto de Azevedo <fazedo@gmail.com>
2020-08-24 09:18:00 -0300

    +imagens

--- figs/helice_dextro.py
+++ figs/helice_dextro.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 
 t = np.linspace(0, 6*np.pi, 201)
 
-x = np.cos(t)*2
-y = np.sin(t)*2
+x = np.cos(t)
+y = np.sin(t)
 z = t
 
 fig = plt.figure()
@@ -15,11 +15,11 @@ ax.plot(x, y, z)
 
 ax.view_init(25, 20) # Elevação e azimute
 
-ax.set_xticks([-2, -1, 0, 1, 2])
-ax.set_yticks([-2, -1, 0, 1, 2])
+#ax.set_xticks([-2, -1, 0, 1, 2])
+#ax.set_yticks([-2, -1, 0, 1, 2])
 ax.set_zticks([0, 5, 10, 15, 20])
 
 #fig.suptitle('Hélice circular uniforme dextrogira')
 fig.set_facecolor('white')
-#plt.show()
-plt.savefig('helice_dextro.png', dpi=200)
+plt.show()
+#plt.savefig('helice_dextro.png', dpi=200)
