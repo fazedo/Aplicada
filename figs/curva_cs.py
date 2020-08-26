d9dbe6bc4b7734406e8a5ddce86598fe4f523874
Fabio Souto de Azevedo <fazedo@gmail.com>
2020-08-26 15:33:29 -0300

    +dia 26

---
+++ figs/curva_cs.py
@@ -0,0 +1,25 @@
+# Plotagem paramétrica de uma curva
+import numpy as np
+import matplotlib.pyplot as plt
+
+t = np.linspace(0, 6*np.pi, 201)
+
+x = np.cos(t)
+y = np.sin(t)
+z = np.sin(2*t)
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+ax.plot(x, y, z)
+
+ax.view_init(25, 20) # Elevação e azimute
+
+#ax.set_xticks([-2, -1, 0, 1, 2])
+#ax.set_yticks([-2, -1, 0, 1, 2])
+#ax.set_zticks([0, 5, 10, 15, 20])
+
+#fig.suptitle('Hélice circular uniforme dextrogira')
+fig.set_facecolor('white')
+#plt.show()
+plt.savefig('curva_cs.png', dpi=200)
