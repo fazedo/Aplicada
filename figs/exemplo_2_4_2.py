37b4bb311ab67609da8716efbfe6839882cf770d
Fabio Souto de Azevedo <fazedo@gmail.com>
2020-08-31 07:58:37 -0300

    +imagens dos exemplos 1, 2 e 3 dda aula 4.

---
+++ figs/exemplo_2_4_2.py
@@ -0,0 +1,28 @@
+# Quiver
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+N = 11
+x = np.linspace(-1, 1, N)
+y = np.linspace(-1, 1, N)
+
+X = np.zeros((N, N), dtype=float)
+Y = np.zeros((N, N), dtype=float)
+F1 = np.zeros((N, N), dtype=float) # Componente x do campo
+F2 = np.zeros((N, N), dtype=float) # Componente y do campo
+
+
+for i in range(N):
+    for j in range(N):
+        X[i, j] = x[i]
+        Y[i, j] = y[j]
+
+        F1[i, j] = x[i]
+        F2[i, j] = 0
+
+fig, ax = plt.subplots()
+ax.quiver(X, Y, F1, F2)
+fig.set_facecolor('white')
+#plt.show()
+fig.savefig('exemplo_2_4_2.png', dpi=200)
